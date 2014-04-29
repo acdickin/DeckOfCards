5e74465606842baa622dc3aa2d11b264f9596f6f
acdickin <acdickin@gmail.com>
2014-04-29 14:48:29 -0700

    Creates a deck of cards
    Classes

---
+++ Deck.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Deck
+    {
+        List<Card> cards;
+        public Deck()
+        {
+
+            cards = new List<Card>();
+            initalizeDeck();
+        }
+
+        private void initalizeDeck()
+        {
+
+            string suit = null;
+
+
+
+            for (int j = 0; j < 4; j++)
+            {
+                switch (j)
+                {
+                    case 0:
+
+                        suit = "Hearts";
+                        break;
+
+                    case 1:
+
+                        suit = "Dimonds";
+                        break;
+
+                    case 2:
+
+                        suit = "Clubs";
+                        break;
+
+                    case 3:
+                        suit = "Spade";
+                        break;
+                }
+                for (int x = 1; x <= 13; x++)
+                {
+                    Card c = new Card();
+                    c.Suit = suit;
+                    c.Num = x;
+                    cards.Add(c);
+
+
+                }
+
+            }
+        }
+        public List<Card> GetCard()
+        {
+            return cards;
+        }
+
+    }
+}
---
+++ DeckofCards.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Deck d = new Deck();
+            List<Card> cards = d.GetCard();
+            foreach  (Card card in cards)
+            {
+                Console.WriteLine(card.Suit + " " + card.Num);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
